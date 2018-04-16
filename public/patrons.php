9d0f4bf6a422627746eff9aefd4130b8080938a1
Samuel Ryan <sam@samryan.co.uk>
2018-04-16 21:11:58 +0100

    Fixes incorrect method reference

--- public/patrons.php
+++ public/patrons.php
@@ -40,7 +40,7 @@ $campaign = $campaign->campaigns()->getMyCampaign();
       <p>
         You are a patron of my campaign, thank you, that means everybody can
         see your picture when they visit this website! You have spent a total of
-        $<?php echo $me->getPledge()->totalSpent(); ?> as my patron.
+        $<?php echo $me->getPledge()->getTotalSpent(); ?> as my patron.
       </p>
       <!-- If the user has a reward -->
       <?php if ($me->getPledge()->hasReward()) { ?>
